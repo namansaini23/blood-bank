c754ada21742c4a52863c50f4621a4fb8f67aa9a
navmeetsekhon <navmeetsekhon@gmail.com>
2024-01-30 21:27:18 +0530

    commit message

---
+++ client/src/Pages/Donor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Donor() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Donor
---
+++ client/src/Pages/Recepient.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recepient() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recepient

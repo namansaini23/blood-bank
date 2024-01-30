4af506bac18f1065e026286a823cdb7b375117b4
navmeetsekhon <navmeetsekhon@gmail.com>
2024-01-30 22:14:12 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 import express  from 'express'
 import cors from 'cors';
 import connectDB from './config/connectdb.js'
 import userRoutes from './routes/userRoutes.js'
 import userModel from './models/user.js';
 const app = express()
-const port = process.env.PORT
+const port = 8080;
 const DATABASE_URL = process.env.DATABASE_URL
 //cors  policy
 app.use(cors())
 
 //databace collection
-connectDB(DATABASE_URL)
+// connectDB(DATABASE_URL)
 
 // jason
 app.use(express.json())
@@ -20,6 +20,9 @@ app.use(express.json())
 // load routes
 app.use("/api/user", userRoutes)
 
+app.get("/heartbeat",(req,res)=>{
+    res.send("server is running at "+port);
+})
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)

429a9bac0dab386739d32689866b3066f262ca3f
namansaini23 <namansaini2307@gmail.com>
2024-02-03 20:59:11 +0530

    password reset on another link api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,12 +10,14 @@ router.use('/loggeduser', checkUserAuth)
 //public routes
 router.post('/register', UserController.userRegistration),
 router.post('/login', UserController.userLogin)
+router.post('/send-reset-password-email', UserController.sendUserPasswordResetEmail)
+router.post('/reset-password/:id/:token', UserController.userPasswordReset)
 
 
 //private routes
 router.post('/changepassword', UserController.changePassword)
 router.get('/loggeduser', UserController.loggedUser)
-
+    
 
 
 export default router

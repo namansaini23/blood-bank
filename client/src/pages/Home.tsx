39a21a27ea88a8cb91db1c2ece6068f8dc2b5827
navmeetsekhon <navmeetsekhon@gmail.com>
2024-02-23 21:19:45 +0530

    client directory update,new react-ts project

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      {/* <NavBar /> */}
+      {/* <Link to advert> */}
+      {/* <Link to ad posting form>*/}
+      {/* <Link to user profile */}
+      {/* <Footer /> */}
+    </div>
+  )
+}
+
+export default Home
